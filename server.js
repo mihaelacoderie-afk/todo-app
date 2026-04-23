602a7e3ef761435c9eb50d6e3cb045791e9550c1
Mihaela <mihaelacoderie@gmail.com>
2026-04-23 22:38:01 +0300

    serve frontend

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
+// frontend static
 app.use(express.static(path.join(__dirname, "frontend")));
 
 app.get("/", (req, res) => {

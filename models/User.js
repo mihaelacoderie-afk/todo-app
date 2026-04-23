8112be24096c099580eaa68813a3a491dccd14db
Mihaela <mihaelacoderie@gmail.com>
2026-04-23 22:46:13 +0300

    fix auth + user model

--- models/User.js
+++ models/User.js
@@ -1,15 +1,23 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
   },
-  password: {
-    type: String,
-    required: true
+  {
+    timestamps: true,
   }
-});
+);
 
 module.exports = mongoose.model("User", userSchema);
